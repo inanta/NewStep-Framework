9a661d53d6df46b5045342807dd950dc43f2703e
Inanta Martsanto <inanta@inationsoft.com>
2015-12-24 09:43:33 +0700

    Update CSV class
    Update CSV class to support read and download.

--- private/libraries/NS/Utility/CSV.class.php
+++ private/libraries/NS/Utility/CSV.class.php
@@ -32,17 +32,94 @@ use NS\Exception\IOException;
 class CSV extends Iterable {
 	private $_columns;
 
-	function __construct($filename, $separator = ',') {
+	function addColumn($column_name) {
+		$this->addColumns($column_name);
+	}
+
+	function addColumns($columns) {
+		if(!is_array($columns)) $columns = array($columns);
+		
+		$column_index = count($this->_columns);
+		
+		foreach($columns as $column) {
+			$this->_columns[$column] = $column_index;
+			++$column_index;
+		}
+	}
+
+	function addData($data) {
+		foreach($data as $datum) {
+			if(!is_array($datum)) {
+				$this->_addSingleData($data);
+				break;
+			}
+
+			$this->_addSingleData($datum);	
+		}
+		
+		parent::__construct();
+	}
+	
+	function download($filename = 'CSV.csv') {
+		$csv = 'sep=;';
+		$csv .= "\r\n";
+
+		foreach($this->_columns as $column => $index) {
+			$csv .= $column . ';';
+		}
+
+		$csv .= "\r\n";
+
+		foreach($this->_collection as $collection) {
+			$csv .= implode(';', $collection);
+			$csv .= "\r\n";
+		}
+
+		header('Content-Description: File Transfer');
+		header('Content-Type: text/csv');
+		header('Content-Disposition: attachment; filename="' . $filename . '"');
+		header('Content-Transfer-Encoding: binary');
+		header('Expires: 0');
+		header('Cache-Control: must-revalidate');
+		header('Pragma: public');
+		header('Content-Length: ' . strlen($csv));
+
+		ob_end_clean();
+		echo $csv;
+
+		exit;
+	}
+		
+	function getColumns() {
+		$columns = array();
+
+		// $value is unused
+		foreach($this->_columns as $key => $value) {
+			$columns[] = $$key;
+		}
+
+		return $columns;
+	}
+
+	function readFile($filename, $separator = ',') {
 		if(!is_readable($filename)) {
 			if(!is_file($filename)) throw new IOException(array('code' => IOException::FILE_NOT_FOUND, 'filename' => $filename));
 
 			throw new IOException(array('code' => IOException::FILE_NOT_READABLE, 'filename' => $filename));
 		}
 
-		$rows = explode("\n", str_replace(array("\n\r", "\r") , "\n", file_get_contents($filename)));
+		$rows = explode("\n", str_replace(array("\n\r", "\r\n", "\r") , "\n", file_get_contents($filename)));
+		$column_row = 0;
+		
+		if(($sep_pos = strpos($rows[$column_row], 'sep=')) !== false) {
+			$separator = substr($rows[$column_row], $sep_pos + 4, 1);
 
-		$columns =  preg_split("/" . $separator . "(?!(?:[^\\\"" . $separator . "]|[^\\\"]" . $separator . "[^\\\"])+\\\")/", $rows[0]);
-		unset($rows[0]);
+			unset($rows[$column_row]);
+			$column_row = 1;
+		}
+
+		$columns =  preg_split("/" . $separator . "(?!(?:[^\\\"" . $separator . "]|[^\\\"]" . $separator . "[^\\\"])+\\\")/", $rows[$column_row]);
+		unset($rows[$column_row]);
 
 		foreach($columns as $index => $column) {
 			$this->_columns[preg_replace("/[^A-Za-z0-9]/", '', $column)] = $index;
@@ -65,16 +142,20 @@ class CSV extends Iterable {
 		parent::__construct();
 	}
 
-	function getColumns() {
-		$columns = array();
+	function _addSingleData($columns) {
+		if(count($columns) != count($this->_columns)) return;
 
-		foreach($this->_columns as $key => $value) {
-			$columns[] = $$key;
+		$data = array();
+		
+		foreach($this->_columns as $column => $index) {
+			// $this->_collection[][$column] = $columns[$index];
+			$data[$column] = $columns[$index];
+		    
 		}
 
-		return $columns;
+		$this->_collection[] = $data;
 	}
-
+		
 	function __get($k) {
 		if(isset($this->_columns[$k])) {
 			return ($this->_collection[$this->_iterator][$k]);
