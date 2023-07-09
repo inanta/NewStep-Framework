0ab27deb51a278095aca4fab3c9ac99a4b655bf5
Inanta Martsanto <inanta@gmail.com>
2023-07-09 14:37:15 +0700

    Change session handler

--- private/libraries/NS/Core/DatabaseSessionHandler.class.php
+++ private/libraries/NS/Core/DatabaseSessionHandler.class.php
@@ -25,14 +25,16 @@ use NS\SingletonObject;
 use NS\Database\Database;
 use NS\Core\Config;
 
-class DatabaseSessionHandler extends SingletonObject {
+class DatabaseSessionHandler extends SingletonObject
+{
 	private $_db;
 
-	function __construct() {
+	function __construct()
+	{
 		$this->createProperty('Lifetime', Config::getInstance()->Application->SessionLifetime == 0 ? 86400 : Config::getInstance()->Application->SessionLifetime);
 		$this->setReadOnlyProperty('Lifetime');
 
-		session_set_save_handler (
+		session_set_save_handler(
 			array(&$this, 'open'),
 			array(&$this, 'close'),
 			array(&$this, 'read'),
@@ -41,65 +43,84 @@ class DatabaseSessionHandler extends SingletonObject {
 			array(&$this, 'gc')
 		);
 
-		if(!isset($_SERVER['HTTP_USER_AGENT'])) $_SERVER['HTTP_USER_AGENT'] = 'NS';
+		if (!isset($_SERVER['HTTP_USER_AGENT']))
+			$_SERVER['HTTP_USER_AGENT'] = 'NS';
 
 		$this->_db = Database::getInstance(Config::getInstance()->Application->SessionBasedDatabaseConenction);
 		register_shutdown_function('session_write_close');
 	}
- 
-	function regenerateID() {
+
+	function regenerateID()
+	{
 		$oldSessionID = session_id();
 		session_regenerate_id();
 		$this->destroy($oldSessionID);
 	}
- 
-	function open($save_path, $name) { return true; } 
-	function close() { return true; }
- 
-	function read($sid) {
-		$result = $this->_db->query("SELECT data FROM " . $this->_db->prefix('sessions') . " WHERE sid = '". $sid . "' AND user_agent = '" . $_SERVER['HTTP_USER_AGENT'] . "' AND expire > '" . time() . "'");
- 
-		if (is_resource($result) && $this->_db->numRows($result) > 0)
-		{
+
+	function open($save_path, $name)
+	{
+		return true;
+	}
+	function close()
+	{
+		return true;
+	}
+
+	function read($sid)
+	{
+		$result = $this->_db->query("SELECT data FROM " . $this->_db->prefix('sessions') . " WHERE sid = '" . $sid . "' AND user_agent = '" . $_SERVER['HTTP_USER_AGENT'] . "' AND expire > '" . time() . "'");
+
+		if ($this->_db->numRows($result) > 0) {
 			$fields = $this->_db->fetchAssoc($result);
 			return $fields["data"];
 		}
- 
+
 		return '';
 	}
- 
-	function write($sid, $data) {		
+
+	function write($sid, $data)
+	{
 		$result = $this->_db->query("SELECT * FROM " . $this->_db->prefix('sessions') . " WHERE sid = '" . $sid . "'");
- 
+
 		if ($this->_db->numRows($result) > 0) {
 			$result = $this->_db->query("UPDATE " . $this->_db->prefix('sessions') . " SET data = '" . $this->_db->escape($data) . "', expire = '" . (time() + $this->Lifetime) . "' WHERE sid = '" . $sid . "'");
- 
-			if ($this->_db->affectedRows()) return true;
+
+			if ($this->_db->affectedRows())
+				return true;
 		} else {
 			$result = $this->_db->query("INSERT INTO " . $this->_db->prefix('sessions') . " (sid, user_agent, data, expire) VALUES ('" . $sid . "','" . $_SERVER['HTTP_USER_AGENT'] . "' , '" . $this->_db->escape($data) . "', '" . (time() + $this->Lifetime) . "')");
- 
-			if ($this->_db->affectedRows()) return true;
+
+			if ($this->_db->affectedRows())
+				return true;
 		}
- 
-		return false;
+
+		return true;
 	}
- 
-	function destroy($sid) {
+
+	function destroy($sid)
+	{
+		return true;
+
 		$result = $this->_db->query("DELETE FROM " . $this->_db->prefix('sessions') . " WHERE sid = '" . $sid . "'");
- 
-		if ($this->_db->affectedRows()) return true;
+
+		if ($this->_db->affectedRows())
+			return true;
 		return false;
 	}
- 
-	function gc($max_lifetime) {
+
+	function gc($max_lifetime)
+	{
 		$result = $this->_db->query("DELETE FROM " . $this->_db->prefix('sessions') . " WHERE expire < '" . (time() - $this->Lifetime) . "'");
 	}
 
 	/**
-	*Create or retrieve object instance
-	*
-	*@return DatabaseSessionHandler
-	*/
-	static function getInstance() { return self::createInstance(__CLASS__); }
+	 *Create or retrieve object instance
+	 *
+	 *@return DatabaseSessionHandler
+	 */
+	static function getInstance()
+	{
+		return self::createInstance(__CLASS__);
+	}
 }
 ?>
