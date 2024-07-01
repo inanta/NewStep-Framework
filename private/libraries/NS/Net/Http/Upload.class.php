d1de7f0789c47f8698a828b15e961046be427886
Inanta Martsanto <inanta@gmail.com>
2024-07-01 16:31:27 +0700

    Add new property and clean code

--- private/libraries/NS/Net/Http/Upload.class.php
+++ private/libraries/NS/Net/Http/Upload.class.php
@@ -1,6 +1,6 @@
 <?php
 /*
-	Copyright (C) 2008 - 2012 Inanta Martsanto
+	Copyright (C) 2008 - 2024 Inanta Martsanto
 	Inanta Martsanto (inanta@inationsoft.com)
 
 	This file is part of NewStep Framework.
@@ -36,62 +36,82 @@ use NS\Exception\IOException;
  *@property string $TemporaryFileName Temporary file name of uploaded file
  *@property string $Type MIME file type of uploaded file
  */
-class Upload extends BaseObject {
+class Upload extends BaseObject
+{
 	private $_postName, $_fileIterator, $_fileNumber;
 
-	function __construct($name, $strict = true) {
-		if(!isset($_FILES[$name]) && $strict) throw new UploadException(array('code' => UploadException::UNDEFINED_FILES, 'variable' => $name));
-		
+	function __construct($name, $strict = true)
+	{
+		if (!isset($_FILES[$name]) && $strict)
+			throw new UploadException(array('code' => UploadException::UNDEFINED_FILES, 'variable' => $name));
+
 		$this->_postName = $name;
 		$this->_fileIterator = -1;
-		
-		$this->createProperties(array(
-			'Error' => 0,
-			'FileExtension' => '',
-			'FileName' => '',
-			'Size' => 0,
-			'TemporaryFileName' => '',
-			'Type' => ''
-		));
 
-		if(@is_array($_FILES[$name]['name'])) $this->_fileNumber = (count($_FILES[$name]['name']) - 1);
+		$this->createProperties(
+			[
+				'Error' => 0,
+				'FileExtension' => '',
+				'FileName' => '',
+				'Name' => '',
+				'Size' => 0,
+				'TemporaryFileName' => '',
+				'Type' => ''
+			]
+		);
+
+		if (@is_array($_FILES[$name]['name']))
+			$this->_fileNumber = (count($_FILES[$name]['name']) - 1);
 
 		$this->next();
 	}
 
-	function save($file = null, $folder = NS_PUBLIC_PATH) {
-		if($file == null) $file = $_FILES[$this->_postName]['name'][$this->_fileIterator];
-		if($_FILES[$this->_postName]['error'][$this->_fileIterator] != UPLOAD_ERR_OK && $_FILES[$this->_postName]['error'][$this->_fileIterator] != UPLOAD_ERR_NO_FILE) throw new UploadException(array('code' => $_FILES[$this->_postName]['error'][$this->_fileIterator]));
-		if(!is_writeable($folder)) throw new IOException(array('code' => IOException::DIRECTORY_NOT_WRITEABLE, 'directory' => $folder));
+	function save($file = null, $folder = NS_PUBLIC_PATH)
+	{
+		if ($file == null)
+			$file = $_FILES[$this->_postName]['name'][$this->_fileIterator];
+		if ($_FILES[$this->_postName]['error'][$this->_fileIterator] != UPLOAD_ERR_OK && $_FILES[$this->_postName]['error'][$this->_fileIterator] != UPLOAD_ERR_NO_FILE)
+			throw new UploadException(array('code' => $_FILES[$this->_postName]['error'][$this->_fileIterator]));
+		if (!is_writeable($folder))
+			throw new IOException(array('code' => IOException::DIRECTORY_NOT_WRITEABLE, 'directory' => $folder));
+
+		// TODO: Why we need this in cPanel, is it cPanel bug?
+		getcwd();
 
 		return move_uploaded_file($_FILES[$this->_postName]['tmp_name'][$this->_fileIterator], $folder . '/' . $file);
 	}
 
-	function hasNext() {
+	function hasNext()
+	{
 		return ($this->_fileIterator <= $this->_fileNumber);
 	}
 
-	function isLast() {
+	function isLast()
+	{
 		return ($this->_fileIterator == $this->_fileNumber);
 	}
 
-	function isFirst() {
+	function isFirst()
+	{
 		return ($this->_fileIterator == 0);
 	}
 
-	function next() {
-		if($this->hasNext()) {
+	function next()
+	{
+		if ($this->hasNext()) {
 			++$this->_fileIterator;
 
+			$filename_part = explode('.', basename($_FILES[$this->_postName]['name'][$this->_fileIterator]));
+			$file_ext = array_pop($filename_part);
+			$name = implode('.', $filename_part);
+
 			$this->Error = $_FILES[$this->_postName]['error'][$this->_fileIterator];
+			$this->FileExtension = $file_ext;
 			$this->FileName = $_FILES[$this->_postName]['name'][$this->_fileIterator];
+			$this->Name = $name;
 			$this->Size = $_FILES[$this->_postName]['size'][$this->_fileIterator];
 			$this->TemporaryFileName = $_FILES[$this->_postName]['tmp_name'][$this->_fileIterator];
 			$this->Type = $_FILES[$this->_postName]['type'][$this->_fileIterator];
-
-			$filename_part = explode('.', $_FILES[$this->_postName]['name'][$this->_fileIterator]);
-			$this->FileExtension = end($filename_part);
 		}
 	}
 }
-?>
